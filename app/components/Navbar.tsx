c3a2dbfca041800d4a8267b79a81d27f46e70733
KSAplay <kennysaavedraaltuna@outlook.com>
2024-11-19 01:12:16 +0100

    Avance en el Sidebar
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,24 +1,19 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
-import { AiOutlineLogout } from "react-icons/ai";
-import { BiSearch } from "react-icons/bi";
-import { IoMdAdd } from "react-icons/io";
-import Logo from "../utils/tiktik-logo.png";
+// import { useRouter } from "next/router";
+// import { GoogleLogin, googleLogout } from "@react-oauth/google";
+// import { AiOutlineLogout } from "react-icons/ai";
+// import { BiSearch } from "react-icons/bi";
+// import { IoMdAdd } from "react-icons/io";
+import Logo from "../utils/tiktik-logo.svg";
 
 export const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-3 px-4">
       <Link href={""}>
-        <div className="w-[100px] md:w-[130px]">
-          <Image
-            className="cursor-pointer"
-            src={Logo}
-            alt="TikTik"
-            layout="intrinsic"
-          />
+        <div className="w-[100px] md:w-[160px]">
+          <Image className="cursor-pointer" src={Logo} alt="TikTik" />
         </div>
       </Link>
     </div>
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,5 +1,90 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+// import { NextPage } from "next";
+// import { useRouter } from "next/router";
+import Link from "next/link";
+import { useGoogleLogin } from "@react-oauth/google";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
+import { Discover } from "./Discover";
+import { SuggestedAccounts } from "./SuggestedAccounts";
+import { Footer } from "./Footer";
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const normalLink =
+    "flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold bg-gradient-to-b from-[#FFA600] to-[#FF007D] bg-clip-text text-transparent";
+  const normalLinkHover = "hover:bg-primary rounded";
+  const userProfile = false;
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onError: () => console.log("Login Failed"),
+  });
+
+  return (
+    <>
+      <div
+        className="block xl:hidden m-2 ml-4 mt-3 text-xl"
+        onClick={() => setShowSidebar((prev) => !prev)}
+      >
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar && (
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={normalLinkHover}>
+                <div className={normalLink}>
+                  <p className="text-2xl">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <defs>
+                        <linearGradient
+                          id="grad"
+                          x1="0%"
+                          y1="0%"
+                          x2="0%"
+                          y2="100%"
+                        >
+                          <stop offset="0%" stopColor="#FFA600" />
+                          <stop offset="100%" stopColor="#FF007D" />
+                        </linearGradient>
+                      </defs>
+                      <AiFillHome fill="url(#grad)" />
+                    </svg>
+                  </p>
+                  <span className="text-xl hidden xl:block">Para ti</span>
+                </div>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">
+                Inicia sesión para dar me gusta y comentar los videos
+              </p>
+              <div className="pr-4">
+                <button
+                  className="cursor-pointer bg-white text-lg text-[#FF007D] border-[1px] border-[#FF007D] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#FF007D]"
+                  onClick={() => login()}
+                >
+                  Iniciar Sesión
+                </button>
+              </div>
+            </div>
+          )}
+
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
+    </>
+  );
 };
