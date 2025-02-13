bdd6c55b382356c931c104d97d9bf17e855757a3
KSAplay <kennysaavedraaltuna@outlook.com>
2025-02-13 03:48:37 +0100

    Suspense en 404
    Signed-off-by: KSAplay <kennysaavedraaltuna@outlook.com>

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
-export default function Custom404() {
+function NotFoundContent() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">404 - Página no encontrada</h1>
@@ -9,5 +10,19 @@ export default function Custom404() {
         <a className="mt-6 text-blue-500 hover:underline">Volver al inicio</a>
       </Link>
     </div>
+  );
+}
+
+export default function Custom404() {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-full w-full">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      }
+    >
+      <NotFoundContent />
+    </Suspense>
   );
 }
